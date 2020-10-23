59d5a162a0b6c5cb89a93c511c4173cd04e6e214
gabrielgog <gabrielgog96@gmail.com>
2020-10-23 22:02:13 +0100

    add route for post page and url lowercase using lodash

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -14,39 +15,40 @@ const posts = []
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
   res.render("home", {
-    startingContent: homeStartingContent, 
+    startingContent: homeStartingContent,
     posts: posts
-    })
+  })
 })
 
 
-app.get("/about", function(req, res){
-  res.render("about", {aboutDescription: aboutContent})
+app.get("/about", function (req, res) {
+  res.render("about", { aboutDescription: aboutContent })
 })
 
 
-app.get("/contact", function(req, res){
-  res.render("contact", {contactDescription: contactContent})
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactDescription: contactContent })
 })
 
 
-app.get("/compose", function(req, res){
+app.get("/compose", function (req, res) {
   res.render("compose")
 
 
 })
 
 
-app.post("/compose", function(req, res){
+
+app.post("/compose", function (req, res) {
 
 
-  const post ={
+  const post = {
 
     title: req.body.compose,
     content: req.body.mainPost
@@ -72,7 +74,30 @@ app.post("/compose", function(req, res){
 
 
 
+app.get("/posts/:postName", function(req, res){
 
-app.listen(3000, function() {
+  const requestedTitle = _.lowerCase(req.params.postName); 
+
+  posts.forEach(function(post){
+
+    const storedTitle = _.lowerCase(post.title);
+    if (storedTitle === requestedTitle){
+
+      res.render("post", {
+
+        title: post.title, 
+        content:post.content
+
+
+      })
+
+
+    } 
+  })
+
+})
+
+
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
